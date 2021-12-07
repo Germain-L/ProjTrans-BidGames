055dbae9adf356e418534234aec89ba36ed8e94e
thomasBarille-ab <thomasbarille@gmail.com>
2021-12-07 14:18:56 +0100

    up

--- src/Controller/DeliveryController.php
+++ src/Controller/DeliveryController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Bids;
 use App\Entity\Delivery;
+use App\Repository\DeliveryRepository;
+use App\Repository\ImagesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -40,5 +42,28 @@ class DeliveryController extends AbstractController
         $normalizers = [new ObjectNormalizer()];
 
         return new Serializer($normalizers, $encoders);
+    }
+
+    #[Route('/delivery', name: 'delivery', methods: 'GET')]
+    public function get_delivery(Request $request, DeliveryRepository $deliveryRepository): Response
+    {
+        $response = new Response();
+
+        $id_entity = json_decode($request->getContent(), true);
+        $deliveryModel = $deliveryRepository->findOneBy(['product' => $id_entity]);
+
+        $serializer = $this->getSerializer();
+
+        $delivery = $serializer->serialize($deliveryModel, 'json');
+
+        if ($delivery) {
+            $response->setContent($delivery);
+        } else {
+            $response->setContent(json_encode([
+                "delvery" => "not found"
+            ]));
+        }
+
+        return $response;
     }
 }
